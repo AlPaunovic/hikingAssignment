b9ef48715115031ca0bc2038fdcdeaa69befcbb8
AlPaunovic <al.paunovic@gmail.com>
2021-09-05 19:40:33 +0200

    about.php i css za about

--- about.php
+++ about.php
@@ -10,10 +10,12 @@
             <img src="img/about.jpg" alt="">
         </article>
         <article>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cupiditate voluptate aut laboriosam maxime officiis nisi eos, deleniti nihil, tenetur dignissimos blanditiis ea distinctio voluptates incidunt perferendis consectetur officia sed possimus modi porro id. Dolor dolorum culpa sed magni? Omnis, eius incidunt! Culpa iusto iste, officia natus quas at consectetur?</p> 
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cupiditate voluptate aut laboriosam maxime officiis nisi eos, deleniti nihil, tenetur dignissimos blanditiis ea distinctio voluptates incidunt perferendis consectetur officia sed possimus modi porro id. Dolor dolorum culpa sed magni? Omnis, eius incidunt! Culpa iusto iste, officia natus quas at consectetur?</p> 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cupiditate voluptate aut laboriosam maxime officiis nisi eos, deleniti nihil, tenetur dignissimos blanditiis ea distinctio voluptates incidunt perferendis consectetur officia sed possimus modi porro id. Dolor dolorum culpa sed magni? Omnis, eius incidunt! Culpa iusto iste, officia natus quas at consectetur?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cupiditate voluptate aut laboriosam maxime officiis nisi eos, deleniti nihil, tenetur dignissimos blanditiis ea distinctio voluptates incidunt perferendis consectetur officia sed possimus modi porro id. Dolor dolorum culpa sed magni? Omnis, eius incidunt! Culpa iusto iste, officia natus quas at consectetur?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cupiditate voluptate aut laboriosam maxime officiis nisi eos, deleniti nihil, tenetur dignissimos blanditiis ea distinctio voluptates incidunt perferendis consectetur officia sed possimus modi porro id. Dolor dolorum culpa sed magni? Omnis, eius incidunt! Culpa iusto iste, officia natus quas at consectetur?</p>
-            <hr><p>Find us on: 
+            <hr class="hr">
+            
+            <p>Find us on: 
         
                 <a href=""><i class="fab fa-facebook"></i></a>
                 <a href=""><i class="fab fa-pinterest"></i></a>
