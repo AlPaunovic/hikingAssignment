09ddabf576acc7bf91e5ec499bc73536da6a15a1
AlPaunovic <al.paunovic@gmail.com>
2021-09-05 17:34:17 +0200

    about.php

--- about.php
+++ about.php
@@ -1,5 +1,30 @@
 <?php include 'header.php'; ?>
 
+<!-- about -->
 
+<section class="about container">
+    <hr>
+    <h2>About us</h2>
+    <div class="row">
+        <article>
+            <img src="img/about.jpg" alt="">
+        </article>
+        <article>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cupiditate voluptate aut laboriosam maxime officiis nisi eos, deleniti nihil, tenetur dignissimos blanditiis ea distinctio voluptates incidunt perferendis consectetur officia sed possimus modi porro id. Dolor dolorum culpa sed magni? Omnis, eius incidunt! Culpa iusto iste, officia natus quas at consectetur?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cupiditate voluptate aut laboriosam maxime officiis nisi eos, deleniti nihil, tenetur dignissimos blanditiis ea distinctio voluptates incidunt perferendis consectetur officia sed possimus modi porro id. Dolor dolorum culpa sed magni? Omnis, eius incidunt! Culpa iusto iste, officia natus quas at consectetur?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cupiditate voluptate aut laboriosam maxime officiis nisi eos, deleniti nihil, tenetur dignissimos blanditiis ea distinctio voluptates incidunt perferendis consectetur officia sed possimus modi porro id. Dolor dolorum culpa sed magni? Omnis, eius incidunt! Culpa iusto iste, officia natus quas at consectetur?</p>
+            <hr><p>Find us on: 
+        
+                <a href=""><i class="fab fa-facebook"></i></a>
+                <a href=""><i class="fab fa-pinterest"></i></a>
+                <a href=""><i class="fab fa-twitter"></i></a>
+                <a href=""><i class="fab fa-instagram"></i></a>
+            
+            </p>
+        </article>
+    </div>
+
+
+</section>
 
 <?php include 'footer.php'; ?>
