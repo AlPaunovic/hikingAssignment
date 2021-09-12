28a068ede34f7415bc4b7807a4a429f6f11ff557
AlPaunovic <al.paunovic@gmail.com>
2021-09-12 21:12:50 +0200

    bezveze promene

--- index.php
+++ index.php
@@ -18,6 +18,8 @@
         </div>
     </header>
 
+
+
 <!-- products -->
 
 <section class="products">
@@ -30,49 +32,58 @@
                 <div class="item">
                     <img src="img/boots.jpg" alt="">
                     <h3>Boots</h3>
-                    <p>$210</p>
+                    <p>&#36; 210</p>
                 </div>
                 <div class="item">
                     <img src="img/pants.jpg" alt="">
                     <h3>Pants</h3>
-                    <p>$40</p>
+                    <p>&#36; 40</p>
                 </div>
                 <div class="item">
                     <img src="img/poles.jpg" alt="">
                     <h3>Poles</h3>
-                    <p>$50</p>
+                    <p>&#36; 65</p>
                 </div>
                 <div class="item">
                     <img src="img/jackets.jpg" alt="">
                     <h3>Jackets</h3>
-                    <p>$50</p>
+                    <p>&#36; 50</p>
                 </div>
                 <div class="item">
                     <img src="img/skirts.jpg" alt="">
                     <h3>Skirts</h3>
-                    <p>$60</p>
+                    <p> &#36; 60</p>
                 </div>
                 <div class="item">
                     <img src="img/shirts.jpg" alt="">
                     <h3>Shirts</h3>
-                    <p>$60</p>
+                    <p>&#36; 60</p>
                 </div>
                 <div class="item">
                     <img src="img/socks.jpg" alt="">
                     <h3>Socks</h3>
-                    <p>$30</p>
+                    <p>&#36; 30</p>
                 </div>
                 <div class="item">
                     <img src="img/gloves.jpg" alt="">
                     <h3>Gloves</h3>
-                    <p>$25</p>
+                    <p>&#36; 25</p>
                 </div>
                 
             </div>
         </section>
 
     </article>
-    </section>
+    </section> 
+
+
+   
+
+
+
+
+
+
 
 <?php include 'partials/footer.php'; ?>
 
@@ -85,7 +96,7 @@
 
 
 
-    <!--  -->
+    
     <!--  -->
     <!--  -->
     
--- products.php
+++ products.php
@@ -1,6 +1,60 @@
 <?php include 'partials/header.php'; ?>
 
+     <!-- producte -->
 
+     <section class="producte container">
+
+     <h1>tabulator</h1>
+         
+        <!-- <div>
+            <div><img src="img/boots.jpg" alt=""></div>
+            <h4>Boots</h4>
+            <p> &#36; 210</p>
+        </div>
+
+        <div>
+            <div><img src="img/pants.jpg" alt=""></div>
+            <h4>Pants</h4>
+            <p> &#36; 40</p>
+        </div>
+
+        <div>
+            <div> <img src="img/poles.jpg" alt=""></div>
+            <h4>Poles</h4>
+            <p> &#36; 50</p>
+        </div>
+
+        <div>
+            <div><img src="img/jackets.jpg" alt=""></div>
+            <h4>Jackets</h4>
+            <p> &#36; 150</p>
+        </div>
+
+        <div>
+            <div><img src="img/skirts.jpg" alt=""></div>
+            <h4>Skirts</h4>
+            <p> &#36; 60</p>
+        </div>
+
+        <div>
+            <div><img src="img/shirts.jpg" alt=""></div>
+            <h4>Shirts</h4>
+            <p> &#36; 30</p>
+        </div>
+
+        <div>
+            <div><img src="img/socks.jpg" alt=""></div>
+            <h4>Socks</h4>
+            <p> &#36; 10</p>
+        </div>
+
+        <div>
+            <div><img src="img/gloves.jpg" alt=""></div>
+            <h4>Gloves</h4>
+            <p> &#36; 25</p>
+        </div> -->
+        
+     </section>
 
 
 <?php include 'partials/footer.php'; ?>
